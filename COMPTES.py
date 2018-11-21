29d79f119f06c8a35b158eecebe3cad9a659e7f1
binetoo <rheny@msn.com>
2018-11-21 17:21:30 +0100

    treize

--- COMPTES.py
+++ COMPTES.py
@@ -3,7 +3,7 @@ from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 import numpy as np
-# 12 12 12 12 
+# 13 13 13 13  
 def VGG_16(weights_path=None):
     model = Sequential()
     model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
